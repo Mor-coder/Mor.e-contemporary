49cfca6eb1ba435a5411ebce22e680e304875ee5
Mor-coder <morshmueli88@gmail.com>
2021-10-20 13:22:03 +0100

    add works to exhibition and divided pages into colms

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 function contactUs(event) {
   event.preventDefault();
   let h4 = document.querySelector("h4");
-  h4.innerHTML = "Thank you for reaching out";
+  h4.innerHTML = "Thank you for reaching out!";
 }
 
-let element = document.querySelector("h4");
-element.addEventListener("click", contactUs);
+let email = document.querySelector("#email");
+email.addEventListener("click", contactUs);
